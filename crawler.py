0db2d45d1a8b3d5c8afbfd67078dc5a7e56745a9
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2023-09-26 00:56:05 -0300

    fix url crawling problem

--- crawler.py
+++ crawler.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 
 import csv
@@ -27,6 +28,8 @@ topics_locator = (By.CLASS_NAME,'forumbg')
 forum_title_locator = (By.CLASS_NAME,'forumtitle')
 topic_title_locator = (By.CLASS_NAME,'topictitle')
 
+wait = WebDriverWait(driver, 10)
+
 csvFile = open('clubeceticismo_data.csv', 'w', newline='', encoding='utf-8')
 writer = csv.writer(csvFile)
 
@@ -38,35 +41,47 @@ def get_wrapper():
   
 def visit_debates_forum():
   allDebates = debatesContainer.find_elements(*forum_title_locator)
-  for debate in allDebates:
-    print('Visiting debate "', debate.text, '"\n')
-    debate.click()
-    visit_topics()
-    driver.back()
-    time.sleep(2)
+  debateTitles = [debate.text for debate in allDebates]
+  debateLinks = [debate.get_attribute("href") for debate in allDebates]
+  for index, link in enumerate(debateLinks):
+    if("https://clubeceticismo.com.br/" in link):
+      print('Visiting debate "', debateTitles[index], '"\n')
+      driver.get(link)
+      time.sleep(3)
+      visit_topics()
+    else:
+      print("Invalid Link")
     
 def visit_topics():
   debatesWrapper = get_wrapper()
   forumTitle = debatesWrapper.find_element(By.CLASS_NAME, 'forum-title').text
   topicsContainer = debatesWrapper.find_element(*topics_locator)
   allTopics = topicsContainer.find_elements(*topic_title_locator)
-  for topic in allTopics:
-    print('Visiting topic "', topic.text, '"\n')
-    topic.click()
-    get_topics_data(forumTitle)
-    driver.back()
-    time.sleep(2)
+  topicTitles = [topic.text for topic in allTopics]
+  topicLinks = [topic.get_attribute("href") for topic in allTopics]
+  for index, link in enumerate(topicLinks):
+    if("https://clubeceticismo.com.br/" in link):
+      print('Visiting topic "', topicTitles[index], '"\n')
+      driver.get(link)
+      time.sleep(3)
+      get_topics_data(forumTitle)
+    else:
+      print("Invalid Link")
 
 # TODO: paginacao
 # tratar posts inválidos
 def get_topics_data(forumTitle):
   url = driver.current_url
   topicsWrapper = get_wrapper()
+  
+  wait = WebDriverWait(driver, timeout=60,ignored_exceptions=[NoSuchElementException])	
+  wait.until(lambda d : driver.find_element(By.CLASS_NAME, "page-body"))	
+  
   topicTitle = topicsWrapper.find_element(By.CLASS_NAME, 'topic-title')
   posts = topicsWrapper.find_elements(By.CLASS_NAME, 'postbody')
   
   for index, post in enumerate(posts):
-    print('Getting topic ', index, ' data\n')
+    print('Getting post', index+1, 'data\n')
     authorElement = post.find_element(By.CLASS_NAME, 'author')
     authorNameElement = authorElement.find_element(By.TAG_NAME, 'strong')
     authorURL = authorNameElement.find_element(By.TAG_NAME, 'a').get_attribute('href')
@@ -92,6 +107,7 @@ debatesContainer = mainWrapper.find_element(*debates_locator)
 
 print("Init crawling...\n")
 visit_debates_forum()
+print("Crawling finished!")
 
 csvFile.close()
 driver.close()
