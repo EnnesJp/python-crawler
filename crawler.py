ab729b466621eabe16475c0948ed02594133b596
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2023-09-25 23:53:25 -0300

    saving data in csv file

--- crawler.py
+++ crawler.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 import csv
 
 chrome_options = Options()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=1920x1080")
 # Remove erro que estava sendo exibido com o chrome
 # Fonte: https://bugs.chromium.org/p/chromedriver/issues/detail?id=2907#c3
@@ -34,56 +34,55 @@ def get_wrapper():
   return driver.find_element(By.ID,'wrap')
   
 def visit_debates_forum():
-  allTags = debatesContainer.find_elements(*forum_title_locator)
-  for tag in allTags:
-    tag.click()
-    get_topics_title()
+  allDebates = debatesContainer.find_elements(*forum_title_locator)
+  for debate in allDebates:
+    debate.click()
+    visit_topics()
     driver.back()
     
-def get_topics_title():
-  newWrapper = get_wrapper()
-  forumTitle = newWrapper.find_element(By.CLASS_NAME, 'forum-title')
-  topicsContainer = newWrapper.find_element(*topics_locator)
-  allTags = topicsContainer.find_elements(*topic_title_locator)
-  for tag in allTags:
-    csv_writer([tag.text, forumTitle.text])
+def visit_topics():
+  debatesWrapper = get_wrapper()
+  forumTitle = debatesWrapper.find_element(By.CLASS_NAME, 'forum-title').text
+  topicsContainer = debatesWrapper.find_element(*topics_locator)
+  allTopics = topicsContainer.find_elements(*topic_title_locator)
+  for topic in allTopics:
+    topic.click()
+    get_topics_data(forumTitle)
+    driver.back()
+
+# TODO: paginacao
+# tratar posts inválidos
+def get_topics_data(forumTitle):
+  url = driver.current_url
+  topicsWrapper = get_wrapper()
+  topicTitle = topicsWrapper.find_element(By.CLASS_NAME, 'topic-title')
+  posts = topicsWrapper.find_elements(By.CLASS_NAME, 'postbody')
+  
+  for post in posts:
+    authorElement = post.find_element(By.CLASS_NAME, 'author')
+    authorNameElement = authorElement.find_element(By.TAG_NAME, 'strong')
+    authorURL = authorNameElement.find_element(By.TAG_NAME, 'a').get_attribute('href')
+    postDateTime = authorElement.find_element(By.TAG_NAME, 'time').get_attribute('datetime')
+    content = post.find_element(By.CLASS_NAME, 'content')
+    
+    csvData = [
+      content.get_attribute('innerHTML'),
+      authorNameElement.text,
+      authorURL,
+      postDateTime,
+      topicTitle.text,
+      forumTitle,
+      url
+    ]
+    
+    csv_writer(csvData)
+    
+  
   
 mainWrapper = get_wrapper()
 debatesContainer = mainWrapper.find_element(*debates_locator)
 
-# visit_debates_forum()
-
-# TODO: paginacao
-# tratar posts inválidos
-def getTopicData(url):
-  driver.get(url)
-  print(url)
-  topicData = dict()
-  topicData['title'] = driver.find_element(By.CLASS_NAME, 'topic-title').text
-  topicData['category']  = driver.find_element(By.ID, 'nav-breadcrumbs').text.replace("\n", ">")
-  topicData['posts'] = []
-
-  posts = driver.find_elements(By.CLASS_NAME, 'postbody')
-  for post in posts:
-    postdata = dict()
-    authorElement = post.find_element(By.CLASS_NAME, 'author')
-    authorNameElement = authorElement.find_element(By.TAG_NAME, 'strong')
-    authorULR = authorNameElement.find_element(By.TAG_NAME, 'a').get_attribute('href')
-    postDateTime = authorElement.find_element(By.TAG_NAME, 'time').get_attribute('datetime')
-    content = post.find_element(By.CLASS_NAME, 'content')
-    
-    postdata['author'] = authorNameElement.text
-    postdata['authorProfile'] = authorULR
-    postdata['postDateTime'] = postDateTime
-    # postdata['content'] = content.get_attribute('innerHTML')
-    topicData['posts'].append(postdata) 
-
-  return topicData
-
-# TODO: parar loop no ultimo post
-for pageNumber in range(1,5):
-  pageUrl = "https://clubeceticismo.com.br/viewtopic.php?t="+str(pageNumber)
-  print(getTopicData(pageUrl))
+visit_debates_forum()
 
 csvFile.close()
 driver.close()
