cb22c497c5a753263f4f14dd14f427e8d461de5d
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2023-09-27 20:56:39 -0300

    fix to get all forums data

--- crawler.py
+++ crawler.py
@@ -23,8 +23,8 @@ assert "No results found." not in driver.page_source
 
 print(driver.title, '\n')
 
-debates_locator = (By.CLASS_NAME,'category-4')
-topics_locator = (By.CLASS_NAME,'forumbg')
+body_locator = (By.CLASS_NAME,'page-body')
+next_page_locator = (By.CLASS_NAME, 'arrow.next')
 forum_title_locator = (By.CLASS_NAME,'forumtitle')
 topic_title_locator = (By.CLASS_NAME,'topictitle')
 currentTopic = 1
@@ -39,48 +39,48 @@ def csv_writer(data):
 
 def get_wrapper():
   return driver.find_element(By.ID,'wrap')
+
+def check_valid_link(link):
+  if ("https://clubeceticismo.com.br/" not in link):
+    print("Invalid Link")
+    return False
+    
+  return True
+
+def visit_forums():
+  allForums = forumsContainer.find_elements(*forum_title_locator)
+  forumTitles = [forum.text for forum in allForums]
+  forumLinks = [forum.get_attribute("href") for forum in allForums]
   
-def visit_debates_forum():
-  allDebates = debatesContainer.find_elements(*forum_title_locator)
-  debateTitles = [debate.text for debate in allDebates]
-  debateLinks = [debate.get_attribute("href") for debate in allDebates]
-  
-  for index, link in enumerate(debateLinks):
-    if("https://clubeceticismo.com.br/" in link):
-      print('Visiting debate "', debateTitles[index], '"\n')
+  for index, link in enumerate(forumLinks):
+    if(check_valid_link(link)):
+      print('Visiting forum "', forumTitles[index], '"\n')
       driver.get(link)
       time.sleep(3)
       visit_topics()
-    else:
-      print("Invalid Link")
 
 def visit_topics(page = 1):
-  print("Getting debate page", page, "\n")
+  print("Getting forum page", page, "\n")
   
   global currentTopic
-  debatesWrapper = get_wrapper()
-  nextPage = debatesWrapper.find_elements(By.CLASS_NAME, 'arrow.next')
-  
-  if nextPage:
-    nextPageUrl = nextPage[0].find_element(By.TAG_NAME,'a').get_attribute("href")
-    
-  forumTitle = debatesWrapper.find_element(By.CLASS_NAME, 'forum-title').text
-  topicsContainer = debatesWrapper.find_element(*topics_locator)
+  forumsWrapper = get_wrapper()
+  forumTitle = forumsWrapper.find_element(By.CLASS_NAME, 'forum-title').text
+  topicsContainer = forumsWrapper.find_element(*body_locator)
+  nextPage = topicsContainer.find_elements(*next_page_locator)
   allTopics = topicsContainer.find_elements(*topic_title_locator)
   topicTitles = [topic.text for topic in allTopics]
   topicLinks = [topic.get_attribute("href") for topic in allTopics]
   
   for index, link in enumerate(topicLinks):
-    if("https://clubeceticismo.com.br/" in link):
+    if(check_valid_link(link)):
       print('Visiting topic "', topicTitles[index], '"\n')
       driver.get(link)
       time.sleep(3)
       get_topics_data(forumTitle)
       currentTopic = currentTopic + 1
-    else:
-      print("Invalid Link")
-      
+
   if nextPage:
+    nextPageUrl = nextPage[0].find_element(By.TAG_NAME,'a').get_attribute("href")
     driver.get(nextPageUrl)
     visit_topics(page + 1)
 
@@ -98,6 +98,7 @@ def get_topics_data(forumTitle, previousIndex = 0, page = 1):
   
   for index, post in enumerate(posts):
     print('Getting post', index+previousIndex+1, 'data\n')
+    
     authorElement = post.find_element(By.CLASS_NAME, 'author')
     authorNameElement = authorElement.find_element(By.TAG_NAME, 'strong')
     authorURL = authorNameElement.find_element(By.TAG_NAME, 'a').get_attribute('href')
@@ -105,7 +106,7 @@ def get_topics_data(forumTitle, previousIndex = 0, page = 1):
     postDateTime = authorElement.find_element(By.TAG_NAME, 'time').get_attribute('datetime')
     content = post.find_element(By.CLASS_NAME, 'content')
     
-    csvData = [
+    csv_writer([
       currentTopic,
       index + previousIndex,
       topicTitle.text,
@@ -116,20 +117,18 @@ def get_topics_data(forumTitle, previousIndex = 0, page = 1):
       authorTotalMessages.text,
       postDateTime,
       content.get_attribute('innerHTML')
-    ]
-    csv_writer(csvData)
+    ])
 
-  nextPage = topicsWrapper.find_elements(By.CLASS_NAME, 'arrow.next')
+  nextPage = topicsWrapper.find_elements(*next_page_locator)
   if nextPage:
-    
     nextPage[0].click()
     get_topics_data(forumTitle, previousIndex + 50, page + 1)
   
 mainWrapper = get_wrapper()
-debatesContainer = mainWrapper.find_element(*debates_locator)
+forumsContainer = mainWrapper.find_element(*body_locator)
 
 print("Init crawling...\n")
-visit_debates_forum()
+visit_forums()
 print("Crawling finished!")
 
 csvFile.close()
